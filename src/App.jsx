c75bdbd281e4fdc8345cc243a1580cdffb7a9ae6
Alyssa Falcione <lyzarddz@gmail.com>
2022-09-09 18:46:42 -0400

    updaed components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import NavBar from './Components/NavBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Stylesheet from './Components/StyleSheet';
+import MainPg from './Components/MainPg';
 import ItemList from './Components/ItemList';
+import { useEffect, useState } from 'react';
+
+
 
 
 function App() {
 
+  const [listLoad, setListLoad] = useState([]);
+  const [itemLoad, setItemLoad] = useState([]);
+
+
+
 
   return (
     <Router>
+      <ItemList/>
 
     <NavBar/>
     <Routes>
-     <Route path="/" element= {<ItemList/>} />
+     <Route path="/" element= {<MainPg/>} />
     </Routes>
     </Router> 
   )
